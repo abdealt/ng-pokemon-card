ff652bca14432e04945034b540b9733ef365cd8e
Abdealt <abdenour@ant-conseil.fr>
2025-01-08 11:37:51 +0100

    Finalisation de la carte | ajout des variable pour la class card

--- src/app/components/playing-card/playing-card.component.ts
+++ src/app/components/playing-card/playing-card.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class PlayingCardComponent {
 
+  name : string = 'my Monster';
+  hp : number = 40;
+  figureCaption : string = 'N°001 Monster';
+  attackName : string = 'Geo Impact';
+  attackStrength : number = 60;
+  attackDescription : string = 'The monster hits the ground with its fist and creates a shockwave that hits the enemy';
 }
